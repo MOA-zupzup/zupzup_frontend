3c7d78d08d7b0232ce4c11d08df3ce1e68cd2985
leejuhi <163210518+leejuhi@users.noreply.github.com>
2025-01-13 22:10:31 +0900

    feat: 로그인 기능 및 fire base 넣기
    추가..

--- ZUPZUP/ZUPZUP/App/LoginView.swift
+++ ZUPZUP/ZUPZUP/App/LoginView.swift
@@ -6,47 +6,52 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct LoginView: View {
     @State private var username: String = ""
     @State private var password: String = ""
+    @State private var loginErrorMessage: String = ""
     
     var body: some View {
-        VStack(alignment:.leading,spacing: 10) {
+        VStack(alignment:.leading, spacing: 10) {
             // 앱 제목
             Text("로그인")
-                .padding(.horizontal,15)
+                .padding(.horizontal, 15)
                 .font(.title2)
                 .fontWeight(.bold)
-                .padding(.top,70)
+                .padding(.top, 70)
             
             // 사용자 이름 입력 필드
             TextField("아이디를 입력해주세요", text: $username)
-                .frame(height:25)
+                .frame(height: 25)
                 .padding()
                 .background(
                     RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.gray, lineWidth: 1) )
+                        .stroke(Color.gray, lineWidth: 1)
+                )
                 .padding(.horizontal, 15)
-                .padding(.top,50)
+                .padding(.top, 50)
+                .autocapitalization(.none) // 이메일 입력 시 대문자 방지
+                .keyboardType(.emailAddress)
             
             // 비밀번호 입력 필드
             SecureField("비밀번호를 입력해주세요", text: $password)
-                .frame(height:25)
+                .frame(height: 25)
                 .padding()
                 .background(
                     RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.gray, lineWidth: 1) )
-
+                        .stroke(Color.gray, lineWidth: 1)
+                )
                 .padding(.horizontal, 15)
-
             
             // 로그인 버튼
             Button(action: {
                 print("로그인 버튼 눌림: \(username), \(password)")
+                loginUser() // 로그인 함수 연결
             }) {
                 Text("로그인")
-                    .frame(height:25)
+                    .frame(height: 25)
                     .font(.headline)
                     .foregroundColor(Color.white)
                     .padding()
@@ -54,31 +59,55 @@ struct LoginView: View {
                     .background(username.isEmpty || password.isEmpty ? Color.gray : Color.black)
                     .cornerRadius(8)
                     .padding(.horizontal, 15)
-                    .padding(.top,25)
+                    .padding(.top, 25)
             }
             .disabled(username.isEmpty || password.isEmpty)
-            //아이디 및 비밀번호 찾기
+            
+            // 로그인 에러 메시지 표시
+            if !loginErrorMessage.isEmpty {
+                Text(loginErrorMessage)
+                    .foregroundColor(.red)
+                    .padding(.horizontal, 15)
+                    .padding(.top, 10)
+            }
+            
+            // 아이디 및 비밀번호 찾기
             HStack(alignment: .center, spacing: 10) {
                 Spacer()
                 Text("아이디 찾기")
                     .foregroundColor(Color.gray)
-                    .padding(.horizontal,10)
+                    .padding(.horizontal, 10)
                 Divider()
-                    .frame(width:1, height: 20)
+                    .frame(width: 1, height: 20)
                     .background(Color.gray)
                 Text("비밀번호 찾기")
                     .foregroundColor(Color.gray)
-                    .padding(.horizontal,10)
+                    .padding(.horizontal, 10)
                 Spacer()
             }
-                .padding(.top,20)
+            .padding(.top, 20)
+            
             Spacer()
         }
         .padding()
+        .ignoresSafeArea(.keyboard) // 키보드 충돌 방지
+    }
+    
+    func loginUser() {
+        Auth.auth().signIn(withEmail: username, password: password) { authResult, error in
+            if let error = error {
+                // 로그인 실패 메시지 출력
+                loginErrorMessage = "로그인 실패! \(error.localizedDescription)"
+                print(loginErrorMessage)
+            } else {
+                // 로그인 성공 메시지 출력
+                loginErrorMessage = ""
+                print("로그인 성공!")
+            }
+        }
     }
 }
 
 #Preview {
     LoginView()
 }
-
--- ZUPZUP/ZUPZUP/App/ZUPZUPApp.swift
+++ ZUPZUP/ZUPZUP/App/ZUPZUPApp.swift
@@ -1,17 +1,34 @@
 //
-//  ZUPZUPApp.swift
+//  AuthProject.swift
 //  ZUPZUP
 //
-//  Created by 강승우 on 1/2/25.
+//  Created by 이주희 on 1/13/25.
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct ZUPZUPApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setupdl
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+          LoginView()
+      }
     }
+  }
 }
+
+
+
